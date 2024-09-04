6a5559a770a24478f105bf60145fc3ce37e9f171
Andydevs <akanshul97@gmail.com>
2024-09-03 20:29:30 -0400

    Create basic SDL app

--- src/ultimate_life/main.cpp
+++ src/ultimate_life/main.cpp
@@ -7,53 +7,41 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-class SDLInstance {
-public:
-    SDLInstance() {
-        if(SDL_Init(SDL_INIT_VIDEO) < 0)
-        {
-            std::ostringstream s;
-            s << "SDL could not initialize! SDL_Error: " << SDL_GetError();
-            throw s.str();
-        }
-        m_window = SDL_CreateWindow("Ultimate Life", 
-                                    SDL_WINDOWPOS_UNDEFINED, 
-                                    SDL_WINDOWPOS_UNDEFINED, 
-                                    SCREEN_WIDTH, SCREEN_HEIGHT, 
-                                    0);
-        if (!m_window)
-        {
-            std::ostringstream s;
-            s << "SDL could not create a window! SDL_Error: " << SDL_GetError();
-            throw s.str();
-        }
-        m_surface = SDL_GetWindowSurface(m_window);
-        if (!m_surface)
-        {
-            std::ostringstream s;
-            s << "SDL could not get window surface! SDL_Error: " << SDL_GetError();
-            throw s.str();
-        }
-        SDL_UpdateWindowSurface(m_window);
-    }
-    ~SDLInstance() throw() {
-        if (m_window) {
-            SDL_DestroyWindow(m_window);
-        }
-    }
-private:
-    SDL_Surface* m_surface;
-    SDL_Window* m_window;
-};
 
 int main(int argc, char const *argv[])
 {
-    try {
-        SDLInstance sdl;
-        SDL_Delay(5000);
+    // Init SDL
+    if(SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        std::cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
     }
-    catch (char* s) {
-        std::cout << s << std::endl;
+
+    // Create window
+    SDL_Window* window = SDL_CreateWindow(
+                              "Ultimate Life", 
+                              SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 
+                              SCREEN_WIDTH, SCREEN_HEIGHT, 
+                              0);
+
+    // Event loop
+    SDL_Event e;
+    bool running = true;
+    while (running)
+    {
+        while (SDL_PollEvent(&e)) {
+            switch (e.type)
+            {
+            case SDL_QUIT:
+                running = false;
+                break;
+            
+            default:
+                break;
+            }
+        }
     }
-    return 0;
+    
+    // Destroy window and quit
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }

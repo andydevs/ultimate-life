2b868f91b13fdc43417ead870be3590dad43a20b
Andydevs <akanshul97@gmail.com>
2024-09-03 20:38:16 -0400

    Rewrite using RAII principle

--- src/ultimate_life/main.cpp
+++ src/ultimate_life/main.cpp
@@ -7,21 +7,40 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
+namespace UL {
+    class App {
+    public:
+        App() {
+            if(SDL_Init(SDL_INIT_VIDEO) < 0)
+            {
+                std::stringstream s;
+                s << "SDL could not initialize! SDL_Error: " << SDL_GetError();
+                throw s.str();
+            }
+        }
+        ~App() {
+            SDL_Quit();
+        }
+    };
+
+    class Window {
+    private:
+        SDL_Window* m_window_handle;
+    public:
+        Window(const char* title, int w, int h) {
+            m_window_handle = SDL_CreateWindow(title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, w, h, 0);
+        }
+        ~Window() {
+            SDL_DestroyWindow(m_window_handle);
+        }
+    };
+};
+
 
 int main(int argc, char const *argv[])
 {
-    // Init SDL
-    if(SDL_Init(SDL_INIT_VIDEO) < 0)
-    {
-        std::cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
-    }
-
-    // Create window
-    SDL_Window* window = SDL_CreateWindow(
-                              "Ultimate Life", 
-                              SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 
-                              SCREEN_WIDTH, SCREEN_HEIGHT, 
-                              0);
+    UL::App app;
+    UL::Window window("Ultimate Life", SCREEN_WIDTH, SCREEN_HEIGHT);
 
     // Event loop
     SDL_Event e;
@@ -40,8 +59,4 @@ int main(int argc, char const *argv[])
             }
         }
     }
-    
-    // Destroy window and quit
-    SDL_DestroyWindow(window);
-    SDL_Quit();
 }

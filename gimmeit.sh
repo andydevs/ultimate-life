9dfcd6acc630e102093c121f9e96e192f6b284ce
Andydevs <akanshul97@gmail.com>
2024-09-09 12:13:13 -0400

    Add antlr step to install script

--- gimmeit.sh
+++ gimmeit.sh
@@ -2,6 +2,7 @@
 build_type=$(conan profile show | grep -E 'build_type=(\w+)' | head -n1 | sed 's/build_type=//')
 conan install . --build=missing
 source build/$build_type/generators/conanbuild.sh
+antlr4 -visitor -no-listener src/lifescript/lifescript.g4
 cmake --preset conan-release
 cmake --build --preset conan-release
 cmake --install build/$build_type

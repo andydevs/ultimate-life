3912b00a4c01510dc7614c4d9efa0b41f7f2499e
Andydevs <akanshul97@gmail.com>
2024-09-09 17:34:07 -0400

    Add install mode config to conan command

--- gimmeit.sh
+++ gimmeit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 build_type=$(conan profile show | grep -E 'build_type=(\w+)' | head -n1 | sed 's/build_type=//')
-conan install . --build=missing
+conan install . --build=missing --conf tools.system.package_manager:mode=install
 source build/$build_type/generators/conanbuild.sh
 antlr4 -visitor -no-listener src/lifescript/lifescript.g4
 cmake --preset conan-release

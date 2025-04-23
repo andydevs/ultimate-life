30782cf92566429aa4f96f529ef5b5a03c3f956e
Andydevs <akanshul97@gmail.com>
2025-04-23 17:48:39 -0400

    Add foreach cell tests

--- tests/test_lifescript_foreachcell.cpp
+++ tests/test_lifescript_foreachcell.cpp
@@ -16,6 +16,7 @@ public:
 };
 
 #define CELL(x, y) LSElem { .prefabbed = false, .prefab_name = "", .elem_cell = cell(x, y) }
+#define PREFAB(name, x, y) LSElem { .prefabbed = true, .prefab_name = name, .elem_cell = cell(x, y) }
 
 TEST(TestLifeScriptForeachCell, TestWithCells) {
     LifeScript2 ls;
@@ -30,5 +31,48 @@ TEST(TestLifeScriptForeachCell, TestWithCells) {
     EXPECT_CALL(f, receive(cell(10, 100)));
 
     // Run foreach cell
+    ls.foreach_cell(f);
+}
+
+TEST(TestLifeScriptForeachCell, TestWithCellAndPrefab) {
+    LifeScript2 ls;
+    MockCellFunctional f;
+
+    vector<LSElem> subelems;
+    subelems.push_back(CELL(3, 4));
+    subelems.push_back(CELL(-3, 2));
+    ls.add_prefab("testprefab", subelems);
+
+    ls.add_elem(CELL(10, 10));
+    ls.add_elem(PREFAB("testprefab", 40, 30));
+
+    EXPECT_CALL(f, receive(cell(10, 10)));
+    EXPECT_CALL(f, receive(cell(43, 34)));
+    EXPECT_CALL(f, receive(cell(37, 32)));
+
+    ls.foreach_cell(f);
+}
+
+TEST(TestLifeScriptForeachCell, TestNestedPrefab) {
+    LifeScript2 ls;
+    MockCellFunctional f;
+
+    vector<LSElem> subsubelems;
+    subsubelems.push_back(CELL(-1,-1));
+    subsubelems.push_back(CELL(-1,0));
+    subsubelems.push_back(CELL(-1,1));
+
+    vector<LSElem> subelems;
+    subelems.push_back(CELL(3, 4));
+    subelems.push_back(CELL(-3, 2));
+    ls.add_prefab("testprefab", subelems);
+
+    ls.add_elem(CELL(10, 10));
+    ls.add_elem(PREFAB("testprefab", 40, 30));
+
+    EXPECT_CALL(f, receive(cell(10, 10)));
+    EXPECT_CALL(f, receive(cell(43, 34)));
+    EXPECT_CALL(f, receive(cell(37, 32)));
+
     ls.foreach_cell(f);
 }

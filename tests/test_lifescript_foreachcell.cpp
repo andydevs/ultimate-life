3144e034cad666fc2a141abda25e8613c09f5461
Andydevs <akanshul97@gmail.com>
2025-04-25 19:41:02 -0400

    Implement test cases for foreach cell

--- tests/test_lifescript_foreachcell.cpp
+++ tests/test_lifescript_foreachcell.cpp
@@ -34,7 +34,7 @@ TEST(TestLifeScriptForeachCell, TestWithCells) {
     ls.foreach_cell(f);
 }
 
-TEST(TestLifeScriptForeachCell, TestWithCellAndPrefab) {
+TEST(TestLifeScriptForeachCell, TestPrefab) {
     LifeScript2 ls;
     MockCellFunctional f;
 
@@ -61,10 +61,11 @@ TEST(TestLifeScriptForeachCell, TestNestedPrefab) {
     subsubelems.push_back(CELL(-1,-1));
     subsubelems.push_back(CELL(-1,0));
     subsubelems.push_back(CELL(-1,1));
+    ls.add_prefab("testsubprefab", subsubelems);
 
     vector<LSElem> subelems;
     subelems.push_back(CELL(3, 4));
-    subelems.push_back(CELL(-3, 2));
+    subelems.push_back(PREFAB("testsubprefab", -3, 9));
     ls.add_prefab("testprefab", subelems);
 
     ls.add_elem(CELL(10, 10));
@@ -72,7 +73,9 @@ TEST(TestLifeScriptForeachCell, TestNestedPrefab) {
 
     EXPECT_CALL(f, receive(cell(10, 10)));
     EXPECT_CALL(f, receive(cell(43, 34)));
-    EXPECT_CALL(f, receive(cell(37, 32)));
+    EXPECT_CALL(f, receive(cell(36, 38))); // 40 30 -> 37 39 -> 36 38
+    EXPECT_CALL(f, receive(cell(36, 39))); // 40 30 -> 37 39 -> 36 39
+    EXPECT_CALL(f, receive(cell(36, 40))); // 40 30 -> 37 39 -> 36 40
 
     ls.foreach_cell(f);
 }

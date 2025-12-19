d4309eb01b6fd076e2ad18817f5a4aee4c92f1f6
Anshul Kharbanda <akanshul97@gmail.com>
2025-12-18 20:11:56 -0500

    Create custom sdl_runtime_error exception class

--- include/raiisdl/error.h
+++ include/raiisdl/error.h
@@ -1,19 +1,41 @@
 #pragma once
 #include <sstream>
 #include <stdexcept>
+#include <string>
 
-#define GUARD_SDL_ERROR(sdl_call, msg)                \
-    if (sdl_call < 0)                                 \
-    {                                                 \
-        std::stringstream s;                          \
-        s << msg << " SDL_Error: " << SDL_GetError(); \
-        throw std::runtime_error(s.str());            \
+namespace raiisdl
+{
+    class sdl_runtime_error : public std::exception
+    {
+    public:
+        sdl_runtime_error(std::stringstream &msg_stream, const char *sdl_err_msg)
+        {
+            m_raiisdl_err_msg = msg_stream.str() + " SDL_ERROR: " + sdl_err_msg;
+        }
+
+        const char *what() const noexcept override
+        {
+            return m_raiisdl_err_msg.c_str();
+        }
+
+    private:
+        std::string m_raiisdl_err_msg;
+    };
+
+} // namespace raiisdl
+
+#define GUARD_SDL_ERROR(sdl_call, msg)                       \
+    if (sdl_call < 0)                                        \
+    {                                                        \
+        std::stringstream s;                                 \
+        s << msg;                                            \
+        throw raiisdl::sdl_runtime_error(s, SDL_GetError()); \
     }
 
-#define ASSERT_SDL_RESOURCE(sdl_resource, msg)        \
-    if (sdl_resource == NULL)                         \
-    {                                                 \
-        std::stringstream s;                          \
-        s << msg << " SDL_Error: " << SDL_GetError(); \
-        throw std::runtime_error(s.str());            \
+#define ASSERT_SDL_RESOURCE(sdl_resource, msg)               \
+    if (sdl_resource == NULL)                                \
+    {                                                        \
+        std::stringstream s;                                 \
+        s << msg;                                            \
+        throw raiisdl::sdl_runtime_error(s, SDL_GetError()); \
     }
